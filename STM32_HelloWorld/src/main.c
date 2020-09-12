24cb3d50546d3199498485244dc33a272b7ec61e
ksekimoto <ksekimoto@users.noreply.github.com>
2020-09-12 22:02:57 +0900

    Added Task related lines

--- STM32_HelloWorld/src/main.c
+++ STM32_HelloWorld/src/main.c
@@ -10,10 +10,42 @@
 
 
 #include "stm32f4xx.h"
-			
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+TaskHandle_t xTaskHandle1 = NULL;
+TaskHandle_t xTaskHandle2 = NULL;
+
+// Task function prototype
+void vTask1_handler(void *params);
+void vTask2_handler(void *params);
 
 int main(void)
 {
+	// 1. Reset RCC clock configuration to default reset state
+	// HCI ON, PLL OFF, system clock = 16MHz, cpu clock = 16MHz
+	RCC_DeInit();
+
+	// 2. update the SystemCoreClock variable
+	SystemCoreClockUpdate();
+
+	// 3. let's create two tasks
+	xTaskCreate(vTask1_handler, "Task-1", configMINIMAL_STACK_SIZE, NULL, 2, &xTaskHandle1);
+	xTaskCreate(vTask2_handler, "Task-2", configMINIMAL_STACK_SIZE, NULL, 2, &xTaskHandle2);
+
+	// 4. Start the scheduler
+	vTaskStartScheduler();
 
 	for(;;);
+}
+
+void vTask1_handler(void *params)
+{
+	while (1);
+}
+
+void vTask2_handler(void *params)
+{
+	while (1);
 }
